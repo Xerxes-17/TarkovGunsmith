e2590235a18d7463d0b42905f433334b285910c2
Xerxes-17 <xerxes17@gmail.com>
2023-03-11 00:10:29 +1100

    Update util.tsx
    Added some API URLs

--- FrontEnd/tarkov-gunsmith/src/Util/util.tsx
+++ FrontEnd/tarkov-gunsmith/src/Util/util.tsx
@@ -1,6 +1,13 @@
 
 // For a deploy, setup the BE first and then get the API URL
 
+// DEV-Local
 //export const API_URL = "http://localhost:5000"
 
+//! PROD
 export const API_URL = "http://wishgranter-prod.eba-mpkq99uq.us-east-1.elasticbeanstalk.com"
+//export const API_URL = "http://api.tarkovgunsmith.com"
+
+//? DEV-TEST
+// export const API_URL = "http://wishgranter-dev-env.eba-wm4piz3d.us-east-1.elasticbeanstalk.com"
+// export const API_URL = "http://api.dev.tarkovgunsmith.com"
