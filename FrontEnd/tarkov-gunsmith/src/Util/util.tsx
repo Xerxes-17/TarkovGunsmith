7a77fc1a2d6532abb0e49d9b38e429210a593b3c
Xerxes-17 <xerxes17@gmail.com>
2023-03-13 11:12:48 +1100

    changed CI pipeline, updated API URL

--- FrontEnd/tarkov-gunsmith/src/Util/util.tsx
+++ FrontEnd/tarkov-gunsmith/src/Util/util.tsx
@@ -5,9 +5,9 @@
 //export const API_URL = "http://localhost:5000"
 
 //! PROD
-export const API_URL = "http://wishgranter-prod.eba-mpkq99uq.us-east-1.elasticbeanstalk.com"
+//export const API_URL = "http://wishgranter-prod.eba-mpkq99uq.us-east-1.elasticbeanstalk.com"
 //export const API_URL = "http://api.tarkovgunsmith.com"
 
 //? DEV-TEST
 // export const API_URL = "http://wishgranter-dev-env.eba-wm4piz3d.us-east-1.elasticbeanstalk.com"
-// export const API_URL = "http://api.dev.tarkovgunsmith.com"
+export const API_URL = "http://api.dev.tarkovgunsmith.com"
