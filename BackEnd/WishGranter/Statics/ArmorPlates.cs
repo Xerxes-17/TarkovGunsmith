76363ff8985a697ec07f9f300bf5eee5a5c522a9
Xerxes-17 <xerxes17@gmail.com>
2024-01-26 12:03:19 +1100

    WIP

---
+++ BackEnd/WishGranter/Statics/ArmorPlates.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using RatStash;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishGranter.Statics
+{
+    // These two record classes are just so the DB can keep consistentcy with their IDs. It's easy enough to just use annotations with them.
+    //? Actually, we're going to use this one to store all of the armorItem details in an SQL table as it's going to exist anyway
+    //todo spin this off into its own .cs file with IEntityTypeConfiguration
+    public record class ArmorPlateOrInsert
+    {
+        [Key]
+        public string Id { get; set; } = "";
+        [Required]
+        public string Name { get; set; } = "";
+        [Required]
+        public int ArmorClass { get; set; }
+        [Required]
+        public ArmorMaterial ArmorMaterial { get; set; }
+        [Required]
+        public float BluntThroughput { get; set; }
+        [Required]
+        public int MaxDurability { get; set; }
+        [Required]
+        public string UsedInId { get; set; } = string.Empty;
+        [Required]
+        public string UsedInName { get; set;} = string.Empty;
+
+        [Required]
+        public List<ArmorPlateCollider> ArmorPlateColliders { get; set; } = new();
+        public List<ArmorCollider> ArmorColliders { get; set; } = new();
+
+        [Required]
+        public string Type { get; set; } = "";
+
+
+        // The input list should be provided by the Armors static class
+        //public static List<ArmorItemStats> GenerateListOfPlatesAndInserts(List<Item> armorList)
+        //{
+        //    var ArmorPlates = armorList.Where(x => x is ArmorPlate).Cast<ArmorPlate>();
+        //    var ArmoredChestRigs = armorList.Where(x => x is ChestRig).Cast<ChestRig>();
+        //    List<ArmorItemStats> outputList = new();
+
+        //    foreach (var armor in ArmoredEquipmentAndChildren)
+        //    {
+        //        TargetZone targetZone;
+        //        if (armor.ArmorZone.Any(x => x.Equals(ArmorZone.Chest)))
+        //        {
+        //            targetZone = TargetZone.Thorax;
+        //        }
+        //        else
+        //            targetZone = TargetZone.Head;
+
+        //        string type;
+        //        if (armor is Headwear)
+        //        {
+        //            type = "Helmet";
+        //        }
+        //        else if (armor is Armor)
+        //        {
+        //            type = "ArmorVest";
+        //        }
+        //        else
+        //        {
+        //            type = "ArmoredEquipment";
+        //        }
+
+        //        ArmorItemStats armorItemStats = new ArmorItemStats
+        //        {
+        //            Id = armor.Id,
+        //            Name = armor.Name,
+        //            ArmorClass = armor.ArmorClass,
+        //            ArmorMaterial = armor.ArmorMaterial,
+        //            BluntThroughput = armor.BluntThroughput,
+        //            MaxDurability = armor.MaxDurability,
+        //            TargetZone = targetZone,
+        //            Type = type,
+        //        };
+        //        outputList.Add(armorItemStats);
+        //    }
+
+        //    foreach (var rig in ArmoredChestRigs)
+        //    {
+        //        ArmorItemStats armorItemStats = new ArmorItemStats
+        //        {
+        //            Id = rig.Id,
+        //            Name = rig.Name,
+        //            ArmorClass = rig.ArmorClass,
+        //            ArmorMaterial = rig.ArmorMaterial,
+        //            BluntThroughput = rig.BluntThroughput,
+        //            MaxDurability = rig.MaxDurability,
+        //            TargetZone = TargetZone.Thorax,
+        //            Type = "ChestRig"
+        //        };
+        //        outputList.Add(armorItemStats);
+        //    }
+
+        //    return outputList;
+        //}
+
+        //public static void Generate_Save_All_ArmorItems()
+        //{
+        //    var items = GenerateListOfArmorItems(Armors.Cleaned);
+
+        //    using var db = new Monolit();
+        //    Console.WriteLine($"Database path: {db.DbPath}.");
+
+        //    foreach (var item in items)
+        //    {
+        //        var check = db.ArmorItems.Any(x => x.Id == item.Id);
+        //        if (check == false)
+        //        {
+        //            db.Add(item);
+        //        }
+        //    }
+        //    db.SaveChanges();
+        //}
+
+        public static ArmorItemStats GetArmorItemStatsByID(string Id)
+        {
+            using var db = new Monolit();
+
+            var item = db.ArmorItems.FirstOrDefault(x => x.Id == Id);
+
+            return item;
+        }
+    }
+}
